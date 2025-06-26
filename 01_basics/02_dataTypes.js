a15942e29a98e713693692213abe92cafeadcac3
Harsh Rajput <161627226+harshrajput2804@users.noreply.github.com>
2025-06-26 17:30:55 +0000

    number and math done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -14,4 +14,11 @@ let scoreInNumber = Number(score) // type conversion Strinng to Number
 console.log(typeof(scoreInNumber));
 console.log(scoreInNumber);
 
+let mark="30";
+let mark1=30;
+
+console.log(mark == mark1); // == compares only values 
+console.log(mark === mark1); // === compares both values and datatypes
+
+
 
