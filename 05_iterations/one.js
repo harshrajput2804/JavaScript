7f3b606aef345b92978ffa962d328407c778a3dc
harshrajput2804 <161627226+harshrajput2804@users.noreply.github.com>
2025-07-07 14:52:42 +0530

    loops done

---
+++ 05_iterations/one.js
@@ -0,0 +1,13 @@
+for (let i = 1; i < 10; i++) {
+    const element = i;
+    if(element == 5){
+        // console.log("i am 5");
+    }
+    // console.log(element);
+}
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(i + '*' + j + '=' + i*j);
+    }
+}
